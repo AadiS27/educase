db3ff80e991c36061b58af49a7bcd6c6ddbcbed5
AadiS27 <Aadisinghal0591@gmail.com>
2025-05-22 12:09:56 +0530

    iidk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, } from 'react-router-dom'
 import  Home  from './pages/home'
 import  Signup  from './pages/signup'
 import Login from './pages/login'
@@ -9,16 +9,15 @@ function App() {
 
   return (
       <BrowserRouter>
-      <div className='bg-white'>
      <Routes>
         <Route path="/" element={<Home/>} />
          <Route path="/signup" element={<Signup/>} />
          <Route path="/login" element={<Login/>} />
          <Route path="/profile" element={<EditProfile/>} />
         </Routes>
-        </div>
+       </BrowserRouter>
 
-      </BrowserRouter>
+      
   )
 }
 
